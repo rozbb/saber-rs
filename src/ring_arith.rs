65b5cd62926791d510d90a3df3a58257ef7e46c4
Michael Rosenberg <michael@mrosenberg.pub>
2024-06-10 17:10:20 +0200

    Removed vec from Karatsuba impl

--- src/ring_arith.rs
+++ src/ring_arith.rs
@@ -59,30 +59,43 @@ impl<'a> Mul for &'a RingElem {
     }
 }
 
-fn poly_add(x: &[u16], y: &[u16]) -> Vec<u16> {
+fn poly_add(x: &[u16], y: &[u16]) -> RingElem {
+    let mut ret = RingElem::default();
     let outlen = core::cmp::max(x.len(), y.len());
-    let mut ret = vec![0; outlen];
     for i in 0..outlen {
-        ret[i] = x.get(i).unwrap_or(&0).wrapping_add(*y.get(i).unwrap_or(&0))
+        ret.0[i] = x.get(i).unwrap_or(&0).wrapping_add(*y.get(i).unwrap_or(&0))
     }
     ret
 }
 
-fn poly_sub(x: &[u16], y: &[u16]) -> Vec<u16> {
+fn poly_sub(x: &[u16], y: &[u16]) -> RingElem {
+    let mut ret = RingElem::default();
     let outlen = core::cmp::max(x.len(), y.len());
-    let mut ret = vec![0; outlen];
     for i in 0..outlen {
-        ret[i] = x.get(i).unwrap_or(&0).wrapping_sub(*y.get(i).unwrap_or(&0))
+        ret.0[i] = x.get(i).unwrap_or(&0).wrapping_sub(*y.get(i).unwrap_or(&0))
     }
     ret
 }
 
-fn kara_mul(x: &[u16], y: &[u16]) -> Vec<u16> {
+fn shift_right(x: &RingElem, shift: usize) -> RingElem {
+    let mut ret = RingElem::default();
+    for i in 0..RING_DEG {
+        let is_neg = ((i + shift) / RING_DEG) % 2 == 1;
+        let coeff = if is_neg { NEG_ONE } else { 1u16 };
+        ret.0[(i + shift) % RING_DEG] = coeff.wrapping_mul(x.0[i]);
+    }
+    ret
+}
+
+// Returns x*y and the size (deg+1) of the resulting polyn
+fn kara_mul(x: &[u16], y: &[u16]) -> RingElem {
     assert_eq!(x.len(), y.len());
     let n = x.len();
 
     if n == 1 {
-        return vec![x[0].wrapping_mul(y[0])];
+        let mut ret = RingElem::default();
+        ret.0[0] = x[0].wrapping_mul(y[0]);
+        return ret;
     }
 
     let mid = n / 2;
@@ -92,31 +105,16 @@ fn kara_mul(x: &[u16], y: &[u16]) -> Vec<u16> {
     let yh = &y[mid..];
 
     let z0 = kara_mul(xl, yl);
-    let mut z2 = kara_mul(xh, yh);
-    let z3 = kara_mul(&poly_add(xl, xh), &poly_add(yl, yh));
-    let mut z1 = poly_sub(&poly_sub(&z3, &z2), &z0);
+    let z2 = kara_mul(xh, yh);
+    let z3 = kara_mul(&poly_add(xl, xh).0[..mid], &poly_add(yl, yh).0[..mid]);
+    let z1 = poly_sub(&poly_sub(&z3.0, &z2.0).0, &z0.0);
 
     // Compute z0 + z1*X^mid + z2*X^(2mid)
-    for _ in 0..mid {
-        z1.insert(0, 0);
-    }
-    for _ in 0..2 * mid {
-        z2.insert(0, 0);
-    }
-    let res = poly_add(&poly_add(&z0, &z1), &z2);
+    let z1 = shift_right(&z1, mid);
+    let z2 = shift_right(&z2, 2 * mid);
+    let res = poly_add(&poly_add(&z0.0, &z1.0).0, &z2.0);
 
-    // Now normalize
-    if res.len() < RING_DEG {
-        res
-    } else {
-        let mut ret = vec![0u16; RING_DEG];
-        for i in 0..res.len() {
-            let is_neg = (i / RING_DEG) % 2 == 1;
-            let coeff = if is_neg { NEG_ONE } else { 1u16 };
-            ret[i % RING_DEG] = ret[i % RING_DEG].wrapping_add(coeff.wrapping_mul(res[i]));
-        }
-        ret
-    }
+    res
 }
 
 impl RingElem {
@@ -177,11 +175,12 @@ mod test {
     fn karatsuba() {
         let mut rng = thread_rng();
 
-        let a = RingElem::rand(&mut rng);
-        let b = RingElem::rand(&mut rng);
+        for _ in 0..100 {
+            let a = RingElem::rand(&mut rng);
+            let b = RingElem::rand(&mut rng);
 
-        let mut k = RingElem::default();
-        k.0.copy_from_slice(&kara_mul(&a.0, &b.0));
-        assert_eq!(k, &a * &b);
+            let k = kara_mul(&a.0, &b.0);
+            assert_eq!(k, &a * &b);
+        }
     }
 }
