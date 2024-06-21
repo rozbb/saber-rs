c6f22d55b35c28047c1728c28a956d0bc55dd76f
Michael Rosenberg <michael@mrosenberg.pub>
2024-06-21 12:40:52 +0200

    Optimized matrix gen a little

--- src/gen.rs
+++ src/gen.rs
@@ -5,7 +5,10 @@ use crate::{
     util::deserialize,
 };
 
-use sha3::{digest::ExtendableOutput, Shake128};
+use sha3::{
+    digest::{ExtendableOutput, Update, XofReader},
+    Shake128,
+};
 
 // Algorithm 16, GenSecret
 /// Uses a random seed to generate an MLWR secret, i.e., an element in R^ℓ whose entries are
@@ -42,22 +45,24 @@ pub(crate) fn gen_secret_from_seed<const L: usize, const MU: usize>(
 // Algorithm 15, GenMatrix
 /// Uses a random seed to generate a uniform matrix in R^{ℓ×ℓ}
 pub(crate) fn gen_matrix_from_seed<const L: usize>(seed: &[u8; 32]) -> Matrix<L, L> {
-    // Make a buffer of the correct length. We can't do const math there, so just make one of the
-    // max length and then cut it down
-    let mut backing_buf = [0u8; MAX_L * MAX_L * RING_DEG * MODULUS_Q_BITS / 8];
-    let buf = &mut backing_buf[..L * L * RING_DEG * MODULUS_Q_BITS / 8];
-
-    // Hash seed to fill up buf with randomness
-    Shake128::digest_xof(&seed, buf);
+    // Hash the seed and make an XOF
+    let mut xof = {
+        let mut h = Shake128::default();
+        h.update(&seed[..]);
+        h.finalize_xof()
+    };
 
     let mut mat = Matrix::default();
-    for (idx, chunk) in buf.chunks(RING_DEG * MODULUS_Q_BITS / 8).enumerate() {
-        // TODO: optimizaiton; we only need to squeeze enough bytes for a single entry at a time
-        let polyn = RingElem::from_bytes(chunk, MODULUS_Q_BITS);
+    let mut buf = [0u8; RING_DEG * MODULUS_Q_BITS / 8];
+    for i in 0..L {
+        for j in 0..L {
+            // For each matrix entry, read some XOF bytes into a buffer and parse it
+            // as a ring elem
+            xof.read(&mut buf);
 
-        let i = idx / L;
-        let j = idx % L;
-        mat.0[i][j] = polyn;
+            let polyn = RingElem::from_bytes(&buf, MODULUS_Q_BITS);
+            mat.0[i][j] = polyn;
+        }
     }
 
     mat
